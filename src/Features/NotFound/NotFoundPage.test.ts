879234f2daf960f1aef59ca083ce3aa19796c068
sand4rt <sander@divotion.com>
2022-07-26 10:35:22 +0200

    fixed failing test

--- src/Features/NotFound/NotFoundPage.test.ts
+++ src/Features/NotFound/NotFoundPage.test.ts
@@ -1,11 +1,16 @@
 import { expect, test } from '../../TestHelpers/TestingLibrary';
 import NotFoundPage from './NotFoundPage.vue';
 
-test.fixme('redirect to the 404 page', async ({ mount, page }) => {
+test('redirect to the 404 page', async ({ mount, page }) => {
 	await mount(NotFoundPage);
 
-	// TODO: page.goto does not work correctly yet with playwright ct testing
-	await page.goto('/route-not-exsist-5b45f7cd193ecc56339c64428079706a');
+	await page.evaluate(() => {
+		window.history.pushState(
+			'',
+			'',
+			'/route-not-exsist-5b45f7cd193ecc56339c64428079706a'
+		);
+	});
 
 	await expect(page).toHaveTitle('404 Page Not Found - TVmaze');
 });
